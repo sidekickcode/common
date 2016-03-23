19643032538c5ded19ddee74c7592e1a41600119
Tim Ruffles <timruffles@gmail.com>
2016-03-23 11:45:07 +0000

    [feature] add more event helpers

--- eventHelpers.js
+++ eventHelpers.js
@@ -1,5 +1,7 @@
 "use strict";
 
+var _ = require("lodash");
+
 exports.proxy = function proxy(proxy, target, originalName, newName) {
   newName = newName || originalName;
 
@@ -11,5 +13,72 @@ exports.proxy = function proxy(proxy, target, originalName, newName) {
 
   function proxied() {
     proxy.emit.apply(proxy, [newName].concat([].slice.call(arguments)).concat(proxy));
+  }
+}
+
+/*::
+
+// null if target is same name as source
+type targetEvent = string | null;
+
+// removes all proxying
+type cleanupFunction = () => null;
+
+*/
+
+exports.proxyMany = function proxyMany(source/*: EventEmitter */, target/*: EventEmitter */, mapping /*: { [id:sourceEvent]: targetEvent } */) /*: cleanupFunction */ {
+
+  var deproxied = false;
+
+  if(!source || !target) {
+    throw new Error("requires two event emitters");
+  }
+
+  mapping = _.clone(mapping);
+
+  var cleanup = _.map(mapping, function(newEvent, original) {
+    newEvent = newEvent == null ? original : newEvent;
+
+    source.addListener(original, handle);
+
+    return function() {
+      source.removeListener(original, handle);
+    };
+
+    function handle() {
+      target.emit.apply(target, [newEvent].concat(_.slice(arguments)));
+    }
+  });
+
+  return function() {
+    _.each(cleanup, _.attempt);
+    mapping = null;
+    cleanup = null;
+  };
+}
+
+exports.proxyAll = function proxyMany(source/*: EventEmitter */, target/*: EventEmitter */, prefix/*: null|string */) /*: cleanupFunction */ {
+
+  var deproxied = false;
+
+  if(!source || !target) {
+    throw new Error("requires two event emitters");
+  }
+
+  var originalEmit = source.emit;
+  source.emit = function(name) {
+    originalEmit.apply(source, arguments);
+    target.emit(prepareName(name), _.tail(arguments));
+  };
+
+  return function() {
+    source.emit = originalEmit;
+    originalEmit = null;
+    source = null;
+    target = null;
+  };
+
+  function prepareName(name) {
+    return prefix == null ? name : prefix + name[0].toUpperCase() + name.slice(1);
   }
 }
